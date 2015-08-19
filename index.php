4d64ca7d92a000ff28246b5508461608dfd6f678
juniorbdb <junior_escjr@yahoo.com.br>
2015-08-19 18:09:46 -0300

    parei no offset5

--- index.php
+++ index.php
@@ -111,6 +111,85 @@
                     .col12
                 </div>
             </div>
+
+            <h1 class="titulo">GRID OFFSET</h1>
+
+            <!-- OFFSET 1 -->
+
+            <div class="row">
+                <div class="col col1 box-custom offset1">
+                    .col1 offset 1
+                </div>
+                <div class="col col1 box-custom offset1">
+                    .col1 offset 1
+                </div>
+                <div class="col col1 box-custom offset1">
+                    .col1 offset 1
+                </div>
+                <div class="col col1 box-custom offset1">
+                    .col1 offset 1
+                </div>
+                <div class="col col1 box-custom offset1">
+                    .col1 offset 1
+                </div>
+                <div class="col col1 box-custom offset1">
+                    .col1 offset 1
+                </div>
+            </div>
+
+            <!-- /OFFSET 1 -->
+
+            <!-- OFFSET 2 -->
+
+            <div class="row">
+                <div class="col col2 box-custom offset2">
+                    .col2 offset 2
+                </div>
+                <div class="col col2 box-custom offset2">
+                    .col2 offset 2
+                </div>
+                <div class="col col2 box-custom offset2">
+                    .col2 offset 2
+                </div>
+            </div>
+
+            <!-- /OFFSET 2 -->
+
+            <!-- OFFSET 3 -->
+
+            <div class="row">
+                <div class="col col3 box-custom offset3">
+                    .col3 offset 3
+                </div>
+                <div class="col col3 box-custom offset3">
+                    .col3 offset 3
+                </div>
+            </div>
+
+            <!-- /OFFSET 3 -->
+
+            <!-- OFFSET 4 -->
+
+            <div class="row">
+                <div class="col col4 box-custom">
+                    .col4 offset 4
+                </div>
+                <div class="col col4 box-custom offset4">
+                    .col4 offset 4
+                </div>
+            </div>
+
+            <!-- /OFFSET 4 -->
+
+            <!-- OFFSET 4 -->
+
+            <div class="row">
+                <div class="col col7 box-custom offset5">
+                    .col7 offset 5
+                </div>
+            </div>
+
+            <!-- /OFFSET 4 -->
         </div>
     </body>
 </html>
