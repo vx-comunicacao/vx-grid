4206152963bf7a92ab7ea257e262053e16fced34
Anderson Nascimento <andersonn@vxcomunicacao.com.br>
2015-08-24 17:34:53 -0300

    Insere viewport > Index

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="css/reset.css" charset="utf-8">
         <link rel="stylesheet" href="css/grid-12-cols.css" charset="utf-8">
         <link rel="stylesheet" href="css/style.css" charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <div class="wrapper">
